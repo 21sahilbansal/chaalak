c93158af0ac69e4f7f042b0fe31bd5584dc36bd9
SahilBansal <sahil.bansal@loconav.com>
2020-03-17 17:17:37 +0530

    single_line_resolved

--- app/src/main/java/com/loconav/locodriver/notification/model/LocoDrivePushNotification.kt
+++ app/src/main/java/com/loconav/locodriver/notification/model/LocoDrivePushNotification.kt
@@ -1,1 +1,27 @@
-package com.loconav.locodriver.notification.modelimport com.google.gson.JsonElementdata class LocoDrivePushNotification(    var body: String?,    var dynamic_link_params: JsonElement?,    var id: Long?,    var meta: JsonElement?,    var mobile_deep_link_path: String?,    var page: String?,    var title: String?,    var ts: Long)data class Meta(    var trip_id: JsonElement?,    var type: String?)data class DynamicLinkParams(    var trip_id: Int?,    var type: String?)data class Expense(    var account_id: Int?,    var account_type: String?,    var amount: Double?,    var creator_id: Int?,    var creator_type: String?,    var creditor_name: String?,    var debitor_id: Int?,    var debitor_name: String?,    var debitor_type: String?,    var expense_date: Long?,    var expense_type: String?,    var id: Int?,    var owner_id: Int?,    var owner_type: String?,    var remarks: String?,    var trip_id: Int?,    var trip_unique_id: String?,    var truck_id: Int?,    var uploads: Uploads?,    var verification_status: String?)data class Uploads(    var expense_document: List<Any?>?)
+package com.loconav.locodriver.notification.model
+
+import com.google.gson.JsonElement
+import com.loconav.locodriver.expense.model.Expense
+
+data class LocoDrivePushNotification(
+    var body: String?,
+    var dynamic_link_params: JsonElement?,
+    var id: Long?,
+    var meta: JsonElement?,
+    var mobile_deep_link_path: String?,
+    var page: String?,
+    var title: String?,
+    var ts: Long
+)
+
+data class Meta(
+    var expense: Expense?,
+    var type: String?
+)
+
+data class DynamicLinkParams(
+    var trip_id: Int?,
+    var type: String?
+)
+
+
--- app/src/main/java/com/loconav/locodriver/notification/model/RegisterFCMDeviceIdConfig.kt
+++ app/src/main/java/com/loconav/locodriver/notification/model/RegisterFCMDeviceIdConfig.kt
@@ -1,1 +1,18 @@
-package com.loconav.locodriver.notification.modelimport com.google.gson.annotations.SerializedNamedata class RegisterFCMDeviceIdConfig(    @SerializedName("token")    var deviceToken: String? = null,    @SerializedName("device_identifier")    var macAddress: String? = null,    @SerializedName("app_version")    var androidVersion: String? = null,    @SerializedName("android_version")    var osVersion: String? = null)
+package com.loconav.locodriver.notification.model
+
+import com.google.gson.annotations.SerializedName
+
+data class RegisterFCMDeviceIdConfig(
+    @SerializedName("token")
+    var deviceToken: String? = null,
+
+    @SerializedName("device_identifier")
+    var macAddress: String? = null,
+
+    @SerializedName("app_version")
+    var androidVersion: String? = null,
+
+    @SerializedName("android_version")
+    var osVersion: String? = null
+)
+

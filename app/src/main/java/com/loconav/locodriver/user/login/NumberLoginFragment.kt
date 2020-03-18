e5380cf2c2cf0ad12ba8096908a86ce804a82760
shivarpit_sharma <shivarpit.sharma@loconav.com>
2020-03-18 19:28:00 +0530

    added check for retrofit error message

--- app/src/main/java/com/loconav/locodriver/user/login/NumberLoginFragment.kt
+++ app/src/main/java/com/loconav/locodriver/user/login/NumberLoginFragment.kt
@@ -15,6 +15,8 @@ import com.google.android.gms.auth.api.credentials.Credential
 import com.google.android.gms.auth.api.credentials.Credentials
 import com.google.android.gms.auth.api.credentials.CredentialsOptions
 import com.google.android.gms.auth.api.credentials.HintRequest
+import com.loconav.locodriver.Constants.RetrofitConstants.Companion.DEFAULT_ERROR_MESSAGE
+import com.loconav.locodriver.Constants.RetrofitConstants.Companion.NO_INTERNET
 import com.loconav.locodriver.R
 import com.loconav.locodriver.base.BaseFragment
 import com.loconav.locodriver.language.LanguageEventBus
@@ -45,16 +47,18 @@ class NumberLoginFragment : BaseFragment() {
 
     override fun onViewInflated(view: View, savedInstanceState: Bundle?) {
         numberLoginViewModel = ViewModelProviders.of(this).get(NumberLoginViewModel::class.java)
-        val pendingIntent = Credentials.getClient(activity!!, options).getHintPickerIntent(hintRequest)
+        val pendingIntent =
+            Credentials.getClient(activity!!, options).getHintPickerIntent(hintRequest)
         button_get_otp.setOnClickListener {
             when {
                 editTextNumber.text.isNullOrEmpty() -> {
                     displayNumberErrorMessage(R.string.blank_number_error_message)
                 }
                 PhoneUtil.isPhoneNumberValid(editTextNumber.text.toString()) -> {
-                    error_message.visibility=View.GONE
-                    editTextNumber.background=ContextCompat.getDrawable(view.context,R.drawable.bg_edittext)
-                    number_login_progressBar.visibility=View.VISIBLE
+                    error_message.visibility = View.GONE
+                    editTextNumber.background =
+                        ContextCompat.getDrawable(view.context, R.drawable.bg_edittext)
+                    number_login_progressBar.visibility = View.VISIBLE
                     requestServerForOtp()
                 }
                 else -> {
@@ -63,20 +67,29 @@ class NumberLoginFragment : BaseFragment() {
             }
         }
 
-        editTextNumber.addTextChangedListener(object :TextWatcher{
+        editTextNumber.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {}
 
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                error_message.visibility= View.INVISIBLE
-                editTextNumber.background=ContextCompat.getDrawable(view.context,R.drawable.bg_edittext)
+                error_message.visibility = View.INVISIBLE
+                editTextNumber.background =
+                    ContextCompat.getDrawable(view.context, R.drawable.bg_edittext)
             }
         })
 
         editTextNumber.setOnFocusChangeListener { _, hasFocus ->
             if (hasFocus && editTextNumber.text.isNullOrBlank())
-                startIntentSenderForResult(pendingIntent.intentSender, PHONE_REQUEST, null, 0, 0, 0, null)
+                startIntentSenderForResult(
+                    pendingIntent.intentSender,
+                    PHONE_REQUEST,
+                    null,
+                    0,
+                    0,
+                    0,
+                    null
+                )
         }
 
         tv_change_language.setOnClickListener {
@@ -86,27 +99,41 @@ class NumberLoginFragment : BaseFragment() {
     }
 
     private fun requestServerForOtp() {
-        numberLoginViewModel?.getOTP(editTextNumber.text.toString())?.observe(this, Observer { dataWrapper ->
-            dataWrapper.data?.let {userDataResponse ->
-                number_login_progressBar.visibility=View.GONE
-                EventBus.getDefault().post(LoginEvent(OPEN_ENTER_OTP_FRAGMENT, editTextNumber.text.toString()))
-                Log.e("variable ", userDataResponse.string())
-            } ?: run{
-                number_login_progressBar.visibility=View.GONE
-                error_message.visibility=View.VISIBLE
-                editTextNumber.background= ContextCompat.getDrawable(editTextNumber.context,R.drawable.error_bg_edit_text)
-                val shake = loadAnimation(context, R.anim.shake)
-                editTextNumber.startAnimation(shake)
-                error_message.text=dataWrapper.throwable?.message
-            }
-        })
+        numberLoginViewModel?.getOTP(editTextNumber.text.toString())
+            ?.observe(this, Observer { dataWrapper ->
+                dataWrapper.data?.let { userDataResponse ->
+                    number_login_progressBar.visibility = View.GONE
+                    EventBus.getDefault()
+                        .post(LoginEvent(OPEN_ENTER_OTP_FRAGMENT, editTextNumber.text.toString()))
+                    Log.e("variable ", userDataResponse.string())
+                } ?: run {
+                    number_login_progressBar.visibility = View.GONE
+                    error_message.visibility = View.VISIBLE
+                    editTextNumber.background = ContextCompat.getDrawable(
+                        editTextNumber.context,
+                        R.drawable.error_bg_edit_text
+                    )
+                    val shake = loadAnimation(context, R.anim.shake)
+                    editTextNumber.startAnimation(shake)
+                    dataWrapper?.throwable?.let {
+                        when {
+                            it.message == NO_INTERNET -> error_message.text =
+                                error_message.context.getString(R.string.no_internet_connection)
+                            it.message == DEFAULT_ERROR_MESSAGE -> error_message.text =
+                                error_message.context.getString(R.string.something_went_wrong)
+                            else -> error_message.text = it.message
+                        }
+                    }
+                }
+            })
     }
 
-    private fun displayNumberErrorMessage(stringResId:Int){
-        val numberEditText=editTextNumber
-        numberEditText.background= ContextCompat.getDrawable(numberEditText.context,R.drawable.error_bg_edit_text)
-        error_message.visibility=View.VISIBLE
-        error_message.text=getString(stringResId)
+    private fun displayNumberErrorMessage(stringResId: Int) {
+        val numberEditText = editTextNumber
+        numberEditText.background =
+            ContextCompat.getDrawable(numberEditText.context, R.drawable.error_bg_edit_text)
+        error_message.visibility = View.VISIBLE
+        error_message.text = getString(stringResId)
         val shake = loadAnimation(context, R.anim.shake)
         numberEditText.startAnimation(shake)
     }
@@ -116,12 +143,12 @@ class NumberLoginFragment : BaseFragment() {
     }
 
     companion object {
-        fun getInstance() : NumberLoginFragment {
+        fun getInstance(): NumberLoginFragment {
             return NumberLoginFragment()
         }
+
         const val PHONE_REQUEST = 4000
     }
-
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -131,7 +158,7 @@ class NumberLoginFragment : BaseFragment() {
                 val credential: Credential? = data.getParcelableExtra(Credential.EXTRA_KEY)
                 val phoneNumber = credential?.id?.removePrefix("+91")
                 editTextNumber.setText(phoneNumber)
-                editTextNumber.setSelection(phoneNumber?.length?:0)
+                editTextNumber.setSelection(phoneNumber?.length ?: 0)
                 // Some of our device fail to get phoneNumber here.
             }
         }

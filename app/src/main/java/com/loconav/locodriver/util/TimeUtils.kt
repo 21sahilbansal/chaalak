636e8a80c88b18785e26232884eea0e918fa3bc0
shivarpit_sharma <shivarpit.sharma@loconav.com>
2020-03-18 22:02:07 +0530

    DRIVE-252

--- app/src/main/java/com/loconav/locodriver/util/TimeUtils.kt
+++ app/src/main/java/com/loconav/locodriver/util/TimeUtils.kt
@@ -8,7 +8,7 @@ class TimeUtils {
     companion object {
         fun getThFormatTime(time: Long): String {
             var finalString: String
-            val date = Date(time)
+            val date = Date(time*1000)
             val n = date.date
             val suffix: String
 
